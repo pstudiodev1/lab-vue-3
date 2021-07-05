e581305687719d6b36f51da614b5daa9decb6b91
pstudiodev <pstudiodev@gmail.com>
2021-07-05 23:13:31 +0700

    Upload image base64

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,11 +4,20 @@
     <a :href="googleUrl">Go to Google</a><br />
     <img :src="imgUrl.url" :alt="imgUrl.alt" v-if="isShow" />
     <button @click="greet">Click me</button>
+    <div>
+      <h1>Upload file</h1>
+      <form @submit.prevent="onSubmit" enctype="multipart/form-data">
+        <input type="file" name="picture" ref="file" />
+        <button type="submit">Upload</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 import { reactive, ref, onMounted, onUnmounted } from "vue";
+import axios from "axios";
+import { BASE_API_URL } from "../constants";
 
 export default {
   name: "About",
@@ -21,11 +30,28 @@ export default {
       alt: "test",
     });
     const isShow = ref(true);
+    const file = ref(null);
+
     const greet = () => {
       // alert("Hello worl");
       email.value = "patt@gmail.com";
       // isShow.value = !isShow.value;
       imgUrl.url = "xxxxx";
+    };
+
+    const onSubmit = async () => {
+      console.log(file.value.files[0]);
+      const fileUpload = file.value.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(fileUpload);
+      reader.onload = async (e) => {
+        const base64Image = e.target.result;
+        console.log(base64Image);
+        const response = await axios.post(`${BASE_API_URL}/api/upload`, {
+          picture: base64Image,
+        });
+        console.log(response.data);
+      };
     };
 
     onMounted(() => {
@@ -36,7 +62,7 @@ export default {
       // console.log("Un Mounted");
     });
 
-    return { email, googleUrl, imgUrl, isShow, greet };
+    return { email, googleUrl, imgUrl, isShow, greet, onSubmit, file };
   },
 };
 </script>
